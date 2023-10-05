ec382eab9eddbcdc7c46fe9514c3df9c6c520ed1
jabz80 <jabzhtc@gmail.com>
2023-10-05 15:13:29 +0100

    html added

---
+++ script.js
@@ -0,0 +1,7 @@
+const todoInput = document.getElementById('task');
+const taskButton = document.getElementById('addTask');
+const todoList = document.getElementById('todoList');
+
+// taskButton.addEventListener('click', () => {
+//     console.log('todo button clicked')
+// })
